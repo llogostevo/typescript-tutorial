b0d85f0e06fea63b2a61044118691e07541b5332
Lloyd <stevens.lloyd@Gmail.com>
2023-05-22 09:57:31 +0000

    tuples

---
+++ tuples.js
@@ -0,0 +1,6 @@
+//create a user array that can have a string or number as values
+// this means that the array can contain either, there is no restriction on order
+var user = [1, "hc"];
+var userAPI = ["LAD", 12, true];
+console.log(userAPI);
+console.log(userAPI[0]);
---
+++ tuples.ts
@@ -0,0 +1,9 @@
+//create a user array that can have a string or number as values
+// this means that the array can contain either, there is no restriction on order
+const user: (string|number)[] = [1, "hc"];
+
+const userAPI: [string,  number, boolean] = ["LAD", 12, true]; 
+
+
+console.log(userAPI);
+console.log(userAPI[0]);
