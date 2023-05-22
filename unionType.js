f1ee540d7df082cc8ff116ffe436bd04061a2148
Lloyd <stevens.lloyd@Gmail.com>
2023-05-22 09:22:25 +0000

    union comments

--- unionType.js
+++ unionType.js
@@ -2,14 +2,20 @@
 var score = 33;
 var person;
 // could be an array
+// set an array of two types which are predefined object literal
 var people = [];
+//push the objects to the array, they can be of either type and it is automatically read by typescript
 people.push({ name: "richard", id: 6 }, { name: "Sarah", id: 1 }, { username: "Sally", id: 6 });
 console.log(people);
+// cycle through each array element
 people.forEach(function (person) {
+    // use an if statement to decide on the type of object, can use an attribute/key check with the in key word
     if ("name" in person) {
+        // this will only accept the attributes/keys from the User object
         console.log("USER: " + person.name + " " + person.id);
     }
     else {
+        // this will only accept the attributes/keys from the admin users
         console.log("ADMIN: " + person.username + " " + person.id);
     }
 });
--- unionType.ts
+++ unionType.ts
@@ -17,15 +17,21 @@ let person: User | Admin;
 
 // could be an array
 
+// set an array of two types which are predefined object literal
 let people: (User|Admin)[] = [];
 
+//push the objects to the array, they can be of either type and it is automatically read by typescript
 people.push({name: "richard", id:6}, {name: "Sarah", id:1}, {username: "Sally", id:6});
 console.log(people)
 
+// cycle through each array element
 people.forEach( (person)=> {
+    // use an if statement to decide on the type of object, can use an attribute/key check with the in key word
     if ("name" in person){
+        // this will only accept the attributes/keys from the User object
         console.log("USER: "+person.name+" "+person.id)
     } else {
+        // this will only accept the attributes/keys from the admin users
         console.log("ADMIN: "+person.username+" "+person.id)
 
     }
